16aa7ef9213baae004c24383c455bcec25121b4c
Aashritha Reddy Kukkala <166220048+aashritha-kr@users.noreply.github.com>
2025-04-15 14:41:39 +0530

    Updated bfs.cu

--- bfs.cu
+++ bfs.cu
@@ -3,15 +3,15 @@
 #include <stdio.h>
 #define N 7
 
-__global__ void kernel(int* visited,int* frontier,int frontier_size,int* g_next_size,int* g_next_frontier,int* level,int* curr_level,int *matrix){
+__global__ void kernel(int* visited,int* layer,int layer_size,int* g_next_size,int* g_next_layer,int* level,int* curr_level,int *matrix){
   int id=blockIdx.x*blockDim.x+threadIdx.x;
-  if(id<frontier_size){
-    int u=frontier[id];
+  if(id<layer_size){
+    int u=layer[id];
     for(int i=0;i<N;++i){
        if (matrix[u * N + i] == 1 && visited[i] == 0) {
         if (atomicExch(&visited[i], 1) == 0) {
         int pos=atomicAdd(g_next_size,1);
-        g_next_frontier[pos]=i;
+        g_next_layer[pos]=i;
         level[i]=*curr_level+1;
       }
     }
@@ -41,22 +41,22 @@ cudaMalloc(&g_visited,N*sizeof(int));
 cudaMemcpy(g_visited,visited,N*sizeof(int),cudaMemcpyHostToDevice);
 
 
-int frontier[N];
-frontier[0]=0;
+int layer[N];
+layer[0]=0;
 visited[0]=1;
-int frontier_size=1;
+int layer_size=1;
 
-int* g_frontier;
-cudaMalloc(&g_frontier,N*sizeof(int));
-cudaMemcpy(g_frontier,frontier,N*sizeof(int),cudaMemcpyHostToDevice);
+int* g_layer;
+cudaMalloc(&g_layer,N*sizeof(int));
+cudaMemcpy(g_layer,layer,N*sizeof(int),cudaMemcpyHostToDevice);
 
-/*int next_frontier[N];*/
-int* g_next_frontier;
-cudaMalloc(&g_next_frontier,N*sizeof(int));
+/*int next_layer[N];*/
+int* g_next_layer;
+cudaMalloc(&g_next_layer,N*sizeof(int));
 
-//int next_frontier_size;
-int* g_next_frontier_size;
-cudaMalloc(&g_next_frontier_size,sizeof(int));
+//int next_layer_size;
+int* g_next_layer_size;
+cudaMalloc(&g_next_layer_size,sizeof(int));
 
 int level[N]={0};
 int* g_level;
@@ -68,22 +68,22 @@ int* g_curr_level;
 cudaMalloc(&g_curr_level,sizeof(int));
 cudaMemcpy(g_curr_level,&curr_level,sizeof(int),cudaMemcpyHostToDevice);
 
-while(frontier_size>0){
+while(layer_size>0){
 
-kernel<<<1, N>>>(g_visited, g_frontier, frontier_size, g_next_frontier_size, g_next_frontier, g_level, g_curr_level, gpumatrix);
+kernel<<<1, N>>>(g_visited, g_layer, layer_size, g_next_layer_size, g_next_layer, g_level, g_curr_level, gpumatrix);
 cudaDeviceSynchronize(); 
 cudaMemcpy(&curr_level,g_curr_level,sizeof(int),cudaMemcpyDeviceToHost);
 curr_level++;
 cudaMemcpy(g_curr_level,&curr_level,sizeof(int),cudaMemcpyHostToDevice);
 
-//now next_frontier becomes frontier
+//now next_layer becomes layer
 
-cudaMemcpy(frontier, g_next_frontier, N * sizeof(int), cudaMemcpyDeviceToHost);
-cudaMemcpy(g_frontier, g_next_frontier, N * sizeof(int), cudaMemcpyDeviceToDevice);
-cudaMemcpy(&frontier_size, g_next_frontier_size, sizeof(int), cudaMemcpyDeviceToHost);
+cudaMemcpy(layer, g_next_layer, N * sizeof(int), cudaMemcpyDeviceToHost);
+cudaMemcpy(g_layer, g_next_layer, N * sizeof(int), cudaMemcpyDeviceToDevice);
+cudaMemcpy(&layer_size, g_next_layer_size, sizeof(int), cudaMemcpyDeviceToHost);
 int zero = 0;
-//next_frontier_size = 0;
-cudaMemcpy(g_next_frontier_size, &zero, sizeof(int), cudaMemcpyHostToDevice);
+//next_layer_size = 0;
+cudaMemcpy(g_next_layer_size, &zero, sizeof(int), cudaMemcpyHostToDevice);
 
 }
 
@@ -97,9 +97,9 @@ for(int i=0;i<N;++i){
 
 cudaFree(gpumatrix);
 cudaFree(g_visited);
-cudaFree(g_frontier);
-cudaFree(g_next_frontier);
-cudaFree(g_next_frontier_size);
+cudaFree(g_layer);
+cudaFree(g_next_layer);
+cudaFree(g_next_layer_size);
 cudaFree(g_level);
 cudaFree(g_curr_level);
 
